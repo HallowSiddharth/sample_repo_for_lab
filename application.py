fc5aba2e84030133afa3f0421d539658746bfa4f
Vishnu Siddharth V R <93894137+HallowSiddharth@users.noreply.github.com>
2024-11-08 08:57:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,20 +1,32 @@
 # This is a sample application
 
-def test_lowercase(data):
-    if data.islower():
-        print("Lower Case: Check", data, "Has Passed the test case")
-    else:
-        print("Lower Case: Check", data, "Has failed the test case")
+with open("results.txt", "w") as report:
 
-def test_uppercase(data):
-    if data.isupper():
-        print("Upper Case: Check", data, "Has Passed the test case")
-    else:
-        print("Upper Case: Check", data, "Has Failed the test case")
+    def test_lowernew(data):
+        print("Checking if data" , data, "is upper case")
+        assert data.isupper()
+
+    def test_lowercase(data):
+        if data.islower():
+            print("Lower Case: Check", data, "Has Passed the test case")
+            report.write(f"LOWER CASE TEST:\t{data}\tPASSED\n")
+        else:
+            print("Lower Case: Check", data, "Has failed the test case")
+            report.write(f"LOWER CASE TEST:\t{data}\tFAILED\n")
+
+    def test_uppercase(data):
+        if data.isupper():
+            print("Upper Case: Check", data, "Has Passed the test case")
+            report.write(f"UPPER CASE TEST:\t{data}\tPASSED\n")
+        else:
+            print("Upper Case: Check", data, "Has Failed the test case")
+            report.write(f"UPPER CASE TEST:\t{data}\tFAILED\n")
+
+    with open("test_data.txt" , "r") as file:
+        datas = file.readlines()
+        for i in datas:
+            #test_lowernew(i)
+            test_lowercase(i)
+            test_uppercase(i)
 
 
-with open("test_data.txt" , "r") as file:
-    datas = file.readlines()
-    for i in datas:
-        test_lowercase(i)
-        test_uppercase(i)
